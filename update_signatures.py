3a20e0c331dcb31dc2fbade845610902266a43f4
Laura W <55563274+Lwaggaman@users.noreply.github.com>
2022-08-05 14:46:45 -0700

    added logging and bulkwrite

--- update_signatures.py
+++ update_signatures.py
@@ -7,6 +7,9 @@ from db_connection import setup_client
 client = setup_client()
 db = client.cc_db
 
+logging.basicConfig(filename='signatures.log',datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s')
+
 def update_signatures():
     """The Governments' transparency site has a csv that contains the names 
     of the Assemblymembers presenting a Norm Proposal.
@@ -17,13 +20,27 @@ def update_signatures():
                             skiprows=[0,14]))
     update._id = update._id.apply(clean_id)
     update = update.to_dict('records')
-    new_sig = db.norms.bulk_write(
-        [UpdateOne({'_id': doc['_id'],'signatures': {'$exists': False}},{'$set': {'signatures':doc['signatures']}}) for doc in update]
-    )
+    
+    #Add signature field to existing docs
+    print('Updating signatures in Norm Proposals from "norms"')
+    try:
+        new_sig = db.norms.bulk_write(
+            [UpdateOne({'_id': doc['_id'],'signatures': {'$exists': False}},{'$set': {'signatures':doc['signatures']}}) for doc in update]
+            )
+        logging.info(f'Signatures added to following docs: {new_sig.bulk_api_result["upserted"]}')
+        print(f'Added signature to {new_sig.matched_count} documents in db')
+    except BulkWriteError as bwe:
+        logging.info(f'Signatures added to following docs: {bwe.details["upserted"]}')
+        for error in bwe.details['writeErrors']:
+            logging.error(f"Failed to add signature: {error['op']['_id']}:{error['errmsg']}")
+        print('Check log: Encountered error when adding signatures to existing docs in db')
+
+
+    #create entire doc if not exists in db 
     for doc in update:
         if not db.norms.find_one({'_id':doc['_id']}):
             db.norms.insert_one(doc)
-    print('Updating signatures in Norm Proposals from "norms"')
+            logging.info(f'New doc "{doc['_id']}" was added to db')
     pprint(new_sig.bulk_api_result)
     if new_sig.modified_count == 0:
         print('Updated: No new documents')
